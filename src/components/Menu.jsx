34c255a94853b834bba1f3c1ee0e38818c0582c3
Your Name <joaopedroortiz11@gmail.com>
2022-03-09 11:55:59 -0300

    Menu iniciado

---
+++ src/components/Menu.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Menu.css';
+
+
+function Menu(){
+return(
+<div className="menu-body">
+<div className="menu-box">
+<div id="close">X</div>
+<p className="menu-text">Notificações</p>
+<div className="divider"></div>
+<p className="menu-text">Notícias</p>
+<div className="divider"></div>
+<p className="menu-text">Manual de Prevenção</p>
+<div className="divider"></div>
+<p className="menu-text">Metas</p>
+<div className="divider"></div>
+<p className="menu-text">Ajuda</p>
+<div className="divider"></div>
+<p className="menu-text">Sair</p></div>
+
+</div>
+)
+}
+
+export default Menu
