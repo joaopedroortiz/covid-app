d270fcef564afcc34170c95c7d5c38923842ed57
MaxSu44 <maxsu44@gmail.com>
2022-03-17 09:41:21 -0300

    Style: Changes in folders and styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+
 import Cadastro from "./Pages/Cadastro/Cadastro.jsx";
-import Menu from "./components/Menu";
+import Menu from "./Pages/Menu/Menu.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route exact path="/" element={<Menu />} />
           <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/" element={<Menu/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import './Menu.css';
+import "./Menu.css";
 
-
-function Menu(){
-return(
-<div className="menu-body">
-<div className="menu-box">
-<div id="close">X</div>
-<p className="menu-text">Notificações</p>
-<div className="divider"></div>
-<p className="menu-text">Notícias</p>
-<div className="divider"></div>
-<p className="menu-text">Manual de Prevenção</p>
-<div className="divider"></div>
-<p className="menu-text">Metas</p>
-<div className="divider"></div>
-<p className="menu-text">Ajuda</p>
-<div className="divider"></div>
-<p className="menu-text">Sair</p></div>
-
-</div>
-)
+function Menu() {
+  return (
+    <div className="menu-body">
+      <div className="menu-box">
+        <div id="close">X</div>
+        <p className="menu-text">Notificações</p>
+        <div className="divider"></div>
+        <p className="menu-text">Notícias</p>
+        <div className="divider"></div>
+        <p className="menu-text">Manual de Prevenção</p>
+        <div className="divider"></div>
+        <p className="menu-text">Metas</p>
+        <div className="divider"></div>
+        <p className="menu-text">Ajuda</p>
+        <div className="divider"></div>
+        <p className="menu-text">Sair</p>
+      </div>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
